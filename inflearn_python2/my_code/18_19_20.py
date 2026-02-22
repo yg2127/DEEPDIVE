2133e42743860df05e85dd837a9ac144c38456c8
YuGeon <yg2127@naver.com>
2026-02-22 19:07:12 +0900

    magic method

--- inflearn_python2/my_code/18_19_20.py
+++ inflearn_python2/my_code/18_19_20.py
@@ -30,4 +30,78 @@ print(l_leng2)
 
 # 네임드 튜플 선언 방법
 
-Point1 = namedtuple('Point', ['x', 'y'])
+Point1 = namedtuple('Point', ['x', 'y']) # 첫 번째 방법 리스트로 선언
+Point2 = namedtuple('Point', 'x, y') # 두 번째 방법 컴마로 구분
+Point3 = namedtuple('Point', 'x y') # 세 번째 방법 띄어쓰기로 선언가능
+Point4 = namedtuple('Point','x y z class', rename = True) # default = False 이다 # 네임드튜플에 사용되는 이름이 예약어인경우 rename을
+# 켜줘서 난수 형태의 변수로 출력된다. p4 출력을 보면 class 이름은 _3으로 되어있다.
+
+print(Point4)
+
+# 출력
+print(Point1, Point2, Point3, Point4)
+
+# Dict 2 unpacking
+temp_dict = {'x' : 75, 'y' : 55}
+
+p1 = Point1(x = 10, y = 35)
+p2 = Point2(20, 40)
+p3 = Point3(45, y = 20)
+p4 = Point4(10, 20, 30, 40)
+p5 = Point1(**temp_dict) #딕셔너리를 언패킹한 후에 네임드튜플에 입력해주면 자동으로 입력된다.
+
+print('-' * 15)
+print(p1)
+print(p2)
+print(p3)
+print(p4) # rename 테스트
+
+print(p1[0] + p2[0])
+print(p1.x + p2.x)
+x, y = p3 # 언패킹
+print(x, y)
+
+# 네임드 튜플 메소드
+
+tmp = [52, 38]
+
+p4 = Point1._make(tmp) # 리스트를 네임드튜플로 변환하는 메소드 = _make
+
+print(p4)
+
+# _fields : 필드 네임 확인
+print(p1._fields, p2._fields, p4._fields)
+
+#_asdict : 정렬된 딕셔너리를 반환
+
+print(p1._asdict(), p2._asdict())
+
+
+# 실 사용 실습
+# 반 20명, 4개의 반(A, B, C, D)
+
+classes = namedtuple('classes', ['rank', 'number'])
+
+# 그룹 리스트 선언
+numbers = [str(n) for n in range(1, 21)] # 1 ~ 20 까지의 str 숫자배열
+ranks = 'A B C D'.split()
+
+print(numbers, ranks)
+
+# List Comprehension
+
+students = [classes(rank, number) for rank in ranks for number in numbers]
+
+print(len(students))
+print(students)
+
+# 추천
+
+students2 = [classes(rank, number)
+             for rank in 'A B C D'.split()
+             for number in [str(n) for n in range(1, 21)]]
+
+print(len(students2))
+
+for s in students2:
+    print(s)
